0774a789243e6e6a65127469813aa01604d95a80
tabish-siraj <tabish1226@gmail.com>
2025-06-19 02:54:29 +0500

    me route

--- src/user/route.ts
+++ src/user/route.ts
@@ -13,10 +13,10 @@ import { authenticateJWT } from '../middlewares/auth';
 const router = Router();
 router.post('/', registerUserController);               // Create user
 router.put('/:id', updateUserController);               // Update user by ID
-router.get('/email/:email', getUserByEmailController);  // Get user by email
-router.get('/:id', getUserByIDController);              // Get user by ID
-router.get('/me', authenticateJWT, getMyUserController); // Get current user profile
 router.put('/profile/:id', updateProfileController);    // Update user profile by ID
+router.get('/email/:email', getUserByEmailController);  // Get user by email
+router.get('/me', authenticateJWT, getMyUserController); // Get current user profile
 router.get('/profile/:id', getProfileController);       // Get user profile by ID
+router.get('/:id', getUserByIDController);              // Get user by ID
 
 export default router;
