e10d4e09b0bbcf70fb0d3c75a7e405b2022de81d
Muhammad Tabish <muhammad.tabish@net2apps.net>
2025-07-09 20:12:10 +0500

    get post by username

--- src/post/route.ts
+++ src/post/route.ts
@@ -12,7 +12,7 @@ const router = Router();
 router.post('/', createPostController);
 router.get('/', getAllPostsController);
 router.get('/:id', getPostByIdController);
-router.get('/username/:username', getPostByIdController);
+router.get('/username/:username', getPostsByUsernameController);
 router.get('/user/:id', getPostsByUserIdController);
 router.delete('/:id', deletePostController);
 
