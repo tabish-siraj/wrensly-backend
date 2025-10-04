a043f326957733be909cd5a1f7888acd17f32f33
tabish-siraj <tabish1226@gmail.com>
2025-10-05 03:16:28 +0500

    bookmark route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,6 +6,7 @@ import commentRoutes from '../comment/route';
 import feedRoutes from '../feed/route';
 import followRoutes from '../follow/route';
 import likeRoutes from '../like/route';
+import bookmarkRoutes from '../bookmark/route';
 import { authenticateJWT } from '../middlewares/auth';
 
 
@@ -18,5 +19,6 @@ router.use('/comment', authenticateJWT, commentRoutes); // /api/comment
 router.use('/feed', authenticateJWT, feedRoutes); // /api/feed
 router.use('/follow', authenticateJWT, followRoutes); // /api/follow
 router.use('/like', authenticateJWT, likeRoutes); // /api/like
+router.use('/bookmark', authenticateJWT, bookmarkRoutes); // /api/bookmark
 
 export default router;
