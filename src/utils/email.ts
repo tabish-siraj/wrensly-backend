f35751e46fda8de95db94c4299cfb64d6168ff6e
tabish-siraj <tabish1226@gmail.com>
2025-10-28 22:24:01 +0500

    email response in stringify

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -14,7 +14,7 @@ const sendEmail = async (options: EmailOptions) => {
       from: '"Wrensly" <onboarding@resend.dev>',
       ...options,
     });
-    logger.info(`Email sent to ${options.to}: ${response}`);
+    logger.info(`Email sent to ${options.to}: ${JSON.stringify(response)}`);
   } catch (error) {
     logger.error(`Error sending email to ${options.to}: ${error}`);
     throw new Error('Error sending email');
