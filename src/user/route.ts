14c367214f05f64ffa66b0fa8b239a7f68fb47ac
tabish-siraj <tabish1226@gmail.com>
2025-10-29 22:27:47 +0500

    post

--- src/user/route.ts
+++ src/user/route.ts
@@ -14,7 +14,7 @@ import { authenticateJWT } from '../middlewares/auth';
 const router = Router();
 router.post('/', registerUserController); // Create user
 router.post('/resend-verify-email', resendVerifyEmailController); // Resend verification email
-router.get('/verify-email', verifyEmailController); // Verify email
+router.post('/verify-email', verifyEmailController); // Verify email
 router.get('/email/:email', authenticateJWT, getUserByEmailController); // Get user by email
 router.get('/username/:username', authenticateJWT, getUserByUsernameController); // Get user by username
 router.get('/me', authenticateJWT, getMyUserController); // Get current user profile
