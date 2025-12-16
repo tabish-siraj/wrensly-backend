6e18257f833a0825ffcd572c05b39ef08d112a80
tabish-siraj <tabish1226@gmail.com>
2025-12-16 21:45:27 +0500

    prisma 7 fully implemented

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,22 @@
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-  log: [
-    { level: 'query', emit: 'stdout' },
-    { level: 'info', emit: 'stdout' },
-    { level: 'warn', emit: 'stdout' },
-    { level: 'error', emit: 'stdout' },
-  ],
-});
+// const prisma = new PrismaClient({
+//   log: [
+//     { level: 'query', emit: 'stdout' },
+//     { level: 'info', emit: 'stdout' },
+//     { level: 'warn', emit: 'stdout' },
+//     { level: 'error', emit: 'stdout' },
+//   ],
+// });
+
+// export default prisma;
+
+import 'dotenv/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../../generated/prisma/client';
+
+const connectionString = process.env.DATABASE_URL;
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import userRoutes from '../user/route';
 import authRoutes from '../auth/route';
 import postRoutes from '../post/route';
-import commentRoutes from '../comment/route';
 import feedRoutes from '../feed/route';
 import followRoutes from '../follow/route';
 import likeRoutes from '../like/route';
@@ -14,7 +13,6 @@ const router = Router();
 router.use('/user', userRoutes); // /api/user
 router.use('/auth', authRoutes); // /api/auth
 router.use('/post', authenticateJWT, postRoutes); // /api/post
-router.use('/comment', authenticateJWT, commentRoutes); // /api/comment
 router.use('/feed', authenticateJWT, feedRoutes); // /api/feed
 router.use('/follow', authenticateJWT, followRoutes); // /api/follow
 router.use('/like', authenticateJWT, likeRoutes); // /api/like
