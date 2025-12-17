59a87b2038d0001a648c0e63a70c8083f5011127
tabish-siraj <tabish1226@gmail.com>
2025-12-17 13:23:49 +0500

    configuration changes

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,19 +6,19 @@ import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../generated/prisma/client';
 
 if (!process.env.DATABASE_URL) {
-    throw new Error('DATABASE_URL is missing');
+  throw new Error('DATABASE_URL is missing');
 }
 
 // Ensure the path is absolute
 const caPath = path.resolve(process.cwd(), 'ca.pem');
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        // This is the critical part for Aiven
-        rejectUnauthorized: true,
-        ca: fs.readFileSync(caPath).toString(),
-    },
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    // This is the critical part for Aiven
+    rejectUnauthorized: true,
+    ca: fs.readFileSync(caPath).toString(),
+  },
 });
 
 // Force the pool to connect once to catch errors early
